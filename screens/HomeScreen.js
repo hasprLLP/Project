9d42eead064dc2af24abe8c6f12d5b1601d0f6cc
rupesh00707 <rupesh007071@gmail.com>
2021-11-23 11:40:36 +0530

    Screens

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
+
+const HomeScreen = ({ navigation }) => {
+    return (
+        <View style={styles.container}>
+            <Text>HomeScreen</Text>
+            </View>
+    );
+};
+
+export default HomeScreen;
