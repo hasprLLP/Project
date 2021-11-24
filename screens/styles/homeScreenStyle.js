a9687d78d73893a7d2b261159de2067c9e30b60d
aadi4899 <84614542+aadi4899@users.noreply.github.com>
2021-11-24 12:39:34 +0530

    Add files via upload

---
+++ screens/styles/homeScreenStyle.js
@@ -0,0 +1,73 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        
+    },
+
+    container_1:{
+        paddingTop: "13%",
+        paddingLeft: "5%"
+    },
+
+    logo: {
+        width: 200,
+        height: 40,
+    },
+
+    container_3: {
+        backgroundColor: 'white',
+        padding: 15,
+        paddingTop: 30,
+    },
+
+    details: {
+        fontSize: 20,
+        paddingLeft: "2%",
+        fontWeight: "700"
+
+    },
+
+    details_1: {
+        fontSize: 13,
+        paddingLeft: "2%",
+        paddingTop: "2%"
+    },
+
+    image: {
+        width: '100%',
+        height: 200
+    },
+
+    container_5: {
+        marginTop: 70,
+        width: "89%",
+        marginLeft: 22,
+    },
+
+    btttn: {
+        height: 40,
+        width: "88%",
+        backgroundColor: '#38b2ac',
+        borderRadius: 10,
+        marginLeft: 15
+    },
+
+    details_3: {
+        fontSize: 10,
+        fontWeight: "200",
+        paddingLeft: 12
+
+    },
+
+    detail_4:
+    {
+        textDecorationLine: "underline",
+        textDecorationStyle: "solid",
+        fontWeight: "200"
+    }
+
+});
+
+export default styles;
